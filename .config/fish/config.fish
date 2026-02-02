c1fa885004522e486957d36aafa194094cbe13ac
Shlri <shirisw3@gmail.com>
2026-02-02 18:27:56 +0300

    setup basics

---
+++ .config/fish/config.fish
@@ -0,0 +1,14 @@
+set -U fish_greeting
+
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	command yazi $argv --cwd-file="$tmp"
+	if read -z cwd < "$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
+
+if status is-interactive
+    starship init fish | source
+end
